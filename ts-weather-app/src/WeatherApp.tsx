ea324cf44326ef8399b248384fc365fa0516b831
Jesus Gastelum <gastelumtec21@gmail.com>
2023-04-23 10:52:46 -0700

    feature: customhook useWeather added

--- ts-weather-app/src/WeatherApp.tsx
+++ ts-weather-app/src/WeatherApp.tsx
@@ -1,40 +1,9 @@
-import { SyntheticEvent, useState } from 'react'
-import { openWeatherInstance, mapboxInstance } from './api';
-import { Feature, MapboxAPI, OpenWeatherAPI, Weather } from './interfaces';
+import { useWeather } from './hooks';
 import { WeatherInformation, WeatherOptions, WeatherSearch } from './components';
 
 export const WeatherApp = () => {
     
-    const [features, setFeatures] = useState<Feature[]>([]);
-    const [weather, setWeather] = useState<Weather>();
-    const [city, setCity] = useState('');
-
-    const handleSearch = async(event: SyntheticEvent, formState: string) => {
-        event.preventDefault();
-        const { data } = await mapboxInstance.get<MapboxAPI>(`${formState}.json`);;
-
-        setCity(formState);
-        setWeather(undefined);
-        setFeatures(data.features);
-    }
-
-    const handleWeather = async(coords: number[]) => {
-        try {
-            const [lat, lon] = coords;
-            const { data } = await openWeatherInstance.get<OpenWeatherAPI>(`weather?lat=${ lat }&lon=${ lon }`);
-            const [ weather ] = data.weather;
-            setWeather(weather);
-        } catch (error) {
-            setWeather({
-                id: 0,
-                description: 'No city information found!',
-                main: 'Nothing to show',
-                icon: 'no-one'
-            })
-        }
-        setFeatures([]);
-
-    }
+    const { features, city, weather, handleSearch, handleWeather } = useWeather();
 
   return (
     <main className='main_container'>
---
+++ ts-weather-app/src/hooks/useWeather.tsx
@@ -0,0 +1,46 @@
+import { useState, SyntheticEvent } from 'react';
+import { Feature, MapboxAPI, OpenWeatherAPI, Weather } from '../interfaces';
+import { mapboxInstance, openWeatherInstance } from '../api';
+
+export const useWeather = () => {
+    const [features, setFeatures] = useState<Feature[]>([]);
+    const [weather, setWeather] = useState<Weather>();
+    const [city, setCity] = useState('');
+
+    const handleSearch = async(event: SyntheticEvent, formState: string) => {
+        
+        event.preventDefault();
+        const { data } = await mapboxInstance.get<MapboxAPI>(`${formState}.json`);;
+
+        setCity(formState);
+        setWeather(undefined);
+        setFeatures(data.features);
+    }
+
+    const handleWeather = async(coords: number[]) => {
+        try {
+            const [lat, lon] = coords;
+            const { data } = await openWeatherInstance.get<OpenWeatherAPI>(`weather?lat=${ lat }&lon=${ lon }`);
+            const [ weather ] = data.weather;
+            setWeather(weather);
+        } catch (error) {
+            setWeather({
+                id: 0,
+                description: 'No city information found!',
+                main: 'Nothing to show',
+                icon: 'no-one'
+            })
+        }
+        setFeatures([]);
+
+    }
+
+    return {
+        features,
+        weather,
+        city,
+        handleSearch,
+        handleWeather
+    }
+
+}
