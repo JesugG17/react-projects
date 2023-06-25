943ba5b5c3ffe2587e159d7c732af6a89cf5047f
Jesus Gastelum <gastelumtec21@gmail.com>
2023-06-24 21:06:19 -0700

    arreglado build de produccion

--- javascript-quiz/src/store/questions.ts
+++ javascript-quiz/src/store/questions.ts
@@ -18,7 +18,7 @@ export const useQuestionsStore = create<State>()(devtools(persist((set, get) => 
         questions: [],
         currentQuestion: 0,
         fetchQuestions: async(limit: number) => {
-           const res = await fetch('http://localhost:5173/data.json');
+           const res = await fetch('https://javascript-quiz-g17.netlify.app/data.json');
            const json = await res.json();
            
            const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
