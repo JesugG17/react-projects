352636b0318d142ee328d0b951edc37da471f96a
Jesus Gastelum <gastelumtec21@gmail.com>
2023-07-08 01:21:10 -0700

    added screen and button to show more info when the mouse enter on a card

--- ts-pokedex/src/components/PokemonGallery.tsx
+++ ts-pokedex/src/components/PokemonGallery.tsx
@@ -1,10 +1,15 @@
-import { FC } from "react";
+import { FC, useState } from 'react';
 import { Pokemon } from "../types/pokemon-interface";
 import { Spinner } from "./Icons";
 import { TYPE_COLOR, TypeKey } from "../utils/color-footer";
 
 export const PokemonGallery: FC<Props> = ({ pokemons, isLoading }) => {
 
+
+  const [showInfo, setShowInfo] = useState({
+    index: 0,
+    show: false
+  });
   
   if (isLoading) {
     return (
@@ -16,10 +21,21 @@ export const PokemonGallery: FC<Props> = ({ pokemons, isLoading }) => {
   return (
     <section className="gallery__container">
       {
-        pokemons.map(pokemon => {
+        pokemons.map((pokemon, index) => {
           const type = pokemon.types[0].type.name as TypeKey;
           return (
-            <div className="pokemon__card" key={pokemon.id}>
+            <div
+              onMouseEnter={() => setShowInfo({
+                show: true,
+                index
+              })}
+              onMouseLeave={() => setShowInfo({
+                show: false,
+                index
+              })}
+              className="pokemon__card" 
+              key={pokemon.id}
+            >
               <figure>
                 <img
                   src={pokemon.sprites.front_default}
@@ -33,6 +49,14 @@ export const PokemonGallery: FC<Props> = ({ pokemons, isLoading }) => {
               >
                 <strong>{pokemon.name}</strong>
               </footer>
+              {
+                showInfo.show && showInfo.index === index && 
+                (
+                  <div className='pokemon__info'>
+                    <button>show more</button>
+                  </div>
+                )
+              }
             </div>
           )
         })
