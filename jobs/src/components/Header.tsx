b6ae70b8ac2a9614bd5d49921ec05956238ced7b
Jesus Gastelum <gastelumtec21@gmail.com>
2023-07-13 22:21:03 -0700

    deleted unnecesary button

--- jobs/src/components/Header.tsx
+++ jobs/src/components/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
         <strong>Rapid</strong> Jobs
       </h2>
       <div className="h-[138px] lg-h[130px] bg-hero bg-center bg-cover flex justify-center items-center rounded-md">
-        <div className="w-4/5 lg:w-3/4 h-2/5 bg-white p-2 flex justify-between items-center rounded">
+        <div className="w-4/5 lg:w-3/4 h-2/5 bg-white p-2 flex items-center rounded">
           <FaSearch />
           <input
             className="w-3/4 bg-transparent focus:outline-none p-2"
@@ -23,9 +23,9 @@ export const Header = () => {
               filterJobs(value.toLowerCase())
             }}
           />
-          <button disabled={ loading } className="w-2/5 lg:w-1/4  h-full bg-search px-4 py-1 rounded-md text-white font-bold hover:bg-search-hover transition-all duration-300">
+          {/* <button disabled={ loading } className="w-2/5 lg:w-1/4  h-full bg-search px-4 py-1 rounded-md text-white font-bold hover:bg-search-hover transition-all duration-300">
             Search
-          </button>
+          </button> */}
         </div>
       </div>
     </header>
