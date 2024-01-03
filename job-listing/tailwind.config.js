89dddc46205aaddf8c317f16f4d61266e7069e82
Jesus Gastelum <gastelumtec21@gmail.com>
2024-01-03 12:13:43 -0800

    initial commit

---
+++ job-listing/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': 'hsl(180, 29%, 50%)',
+        'neutral-grayish-bg': 'hsl(180, 52%, 96%)',
+        'neutral-grayish-filter': 'hsl(180, 31%, 95%)',
+        'dark-grayish-cyan': 'hsl(180, 8%, 52%)',
+        'very-dark-grayish-cyan': 'hsl(180, 14%, 20%)'
+      }
+    },
+  },
+  plugins: [],
+}
+
